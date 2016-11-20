8c52ccca97b52a32e4835c85456f4f472fafe0a8
Francisco Sanchez <sanchezpaco@Franciscos-MacBook-Pro.local>
2016-11-20 20:23:43 +0100

    Changed geoip api

--- app/modules/iplocation_module.rb
+++ app/modules/iplocation_module.rb
@@ -1,7 +1,7 @@
 module IpLocationModule
   def self.get_location_from_ip(ip)
-    response = HttpModule.get('freegeoip.net', "/json/#{ip}")
+    response = HttpModule.get('ipinfo.io', "/#{ip}/json")
     data = ::JSON.parse(response.body)
-    data['country_name']
+    data['city']
   end
 end
